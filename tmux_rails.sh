14da3400f96ec1406c3ab302e72bbef2e36389e7
Joan Roig <roigarderiu@gmail.com>
2015-12-21 15:25:57 -0800

    added tmux railz

---
+++ tmux_rails.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+CURRENT_SESSION=${PWD##*/}
+
+# Start up the tmux session with specific name
+tmux new-session -d -s rails-teespring
+
+# VIM window
+tmux send-keys 'vim' 'C-m'
+tmux rename-window code
+
+# Window for running development server
+tmux new-window
+tmux send-keys 'foreman start -f Procfile.dev' 'C-m'
+tmux rename-window server
+
+# Window for rails console
+tmux new-window
+tmux send-keys 'bundle exec rails c' 'C-m'
+tmux split-window -v
+tmux send-keys 'zeus start' 'C-m'
+tmux rename-window console
+
+# Select the first window
+tmux select-window -t 0
+
+# Attach the tmux session
+tmux attach -t $CURRENT_SESSION
